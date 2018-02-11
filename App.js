240eef6a4239d56825208b4d3b34dce59d8e423c
Unknown <lxv8686@rit.edu>
2018-02-10 22:05:42 -0500

    Camera Tutorial
    idk

--- App.js
+++ App.js
@@ -5,6 +5,7 @@
  */
 
 import React, { Component } from 'react';
+import Camera from 'react-native-camera';
 import {
   Platform,
   StyleSheet,
@@ -23,36 +24,40 @@ type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
+      <Camera
+   ref={(cam) => {
+       this.camera = cam;
+    }}
+    style={styles.preview}
+    aspect={Camera.constants.Aspect.fill}>
+       <Text style={styles.capture} onPress={this.takePicture.bind(this)}>
+          [CAPTURE]
+       </Text>
+</Camera>
     );
-  }
+    takePicture() {
+      this.camera.capture()
+         .then((data) => console.log(data))
+         .catch(err => console.error(err));
+}
+const styles = StyleSheet.create({
+preview: {
+  flex: 1,
+  justifyContent: 'flex-end',
+  alignItems: 'center',
+  height: Dimensions.get('window').height,
+  width: Dimensions.get('window').width
+},
+capture: {
+  flex: 0,
+  backgroundColor: #00eeee,
+  borderRadius: 5,
+  color: '#000',
+  padding: 10,
+  margin: 40
+}
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+
+
+
