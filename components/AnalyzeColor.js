e257f2fd71184d7f26a2798beef62ccf234f12f8
Clarissa Xue <xue.clarissa@icloud.com>
2018-02-10 22:40:21 -0500

    Stub getComplimentaryColor
    (+) AnalyzeColor.js
    stubbed out function for linda to put in code

---
+++ components/AnalyzeColor.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class AnalyzeColor extends Component {
+
+    state = {
+        text: '',
+    }
+
+    onGetComplimentaryColor = (hex) => {
+        //insert code here @Linda
+    }
+
+}
