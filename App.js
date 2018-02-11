3aff7a16648800f026bed2dce1ad5b94286efb20
Clarissa Xue <xue.clarissa@icloud.com>
2018-02-11 00:42:27 -0500

    edit camera tutorial
    needs to be tested

--- App.js
+++ App.js
@@ -1,63 +1,57 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
-import Camera from 'react-native-camera';
 import {
-  Platform,
+  AppRegistry,
   StyleSheet,
   Text,
+  Dimensions,
   View
 } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import Camera from 'react-native-camera';
 
-type Props = {};
-export default class App extends Component<Props> {
+export default class MyCamera extends Component {
   render() {
     return (
-      <Camera
-   ref={(cam) => {
-       this.camera = cam;
-    }}
-    style={styles.preview}
-    aspect={Camera.constants.Aspect.fill}>
-       <Text style={styles.capture} onPress={this.takePicture.bind(this)}>
-          [CAPTURE]
-       </Text>
-</Camera>
+      <View style={styles.container}>
+       <Camera
+           ref={(cam) => {
+             this.camera = cam;
+           }}
+           style={styles.preview}
+           aspect={Camera.constants.Aspect.fill}>
+           <Text style={styles.capture} onPress={this.takePicture.bind(this)}>[CAPTURE]</Text>
+       </Camera>
+      </View>
     );
-    takePicture() {
-      this.camera.capture()
-         .then((data) => console.log(data))
-         .catch(err => console.error(err));
+  }
+
+  takePicture() {
+   this.camera.capture()
+     .then((data) => console.log(data))
+     .catch(err => console.error(err));
+ }
 }
+
 const styles = StyleSheet.create({
-preview: {
-  flex: 1,
-  justifyContent: 'flex-end',
-  alignItems: 'center',
-  height: Dimensions.get('window').height,
-  width: Dimensions.get('window').width
-},
-capture: {
-  flex: 0,
-  backgroundColor: #00eeee,
-  borderRadius: 5,
-  color: '#000',
-  padding: 10,
-  margin: 40
-}
-}
-
-
-
-
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  preview: {
+   flex: 1,
+   justifyContent: 'flex-end',
+   alignItems: 'center',
+   height: Dimensions.get('window').height,
+   width: Dimensions.get('window').width
+ },
+  capture: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    color: '#000',
+    padding: 10,
+    margin: 40
+  }
+});
